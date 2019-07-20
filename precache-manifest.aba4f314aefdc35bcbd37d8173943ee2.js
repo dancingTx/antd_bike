92165a98431cb34060962836ce295e63646cf295
leo_nameless <1150328467@qq.com>
2019-07-20 15:57:28 +0800

    Updates

---
+++ precache-manifest.aba4f314aefdc35bcbd37d8173943ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf95a4133ea922ea4e97653ed39cf28",
+    "url": "/ytx1150328467/antd_bike/index.html"
+  },
+  {
+    "revision": "b7ce8b9b2b8adbe16c21",
+    "url": "/ytx1150328467/antd_bike/static/css/2.b6ea04d8.chunk.css"
+  },
+  {
+    "revision": "f047760f1eedc9f8b771",
+    "url": "/ytx1150328467/antd_bike/static/css/main.62f738ed.chunk.css"
+  },
+  {
+    "revision": "b7ce8b9b2b8adbe16c21",
+    "url": "/ytx1150328467/antd_bike/static/js/2.b9756080.chunk.js"
+  },
+  {
+    "revision": "f047760f1eedc9f8b771",
+    "url": "/ytx1150328467/antd_bike/static/js/main.26f32372.chunk.js"
+  },
+  {
+    "revision": "6c99f0423228a3abdac2",
+    "url": "/ytx1150328467/antd_bike/static/js/runtime~main.e01f491f.js"
+  }
+]);
