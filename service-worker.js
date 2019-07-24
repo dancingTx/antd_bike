4f262c56606b3550119edc380572ad0a2161800b
leo_nameless <1150328467@qq.com>
2019-07-24 21:52:39 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/antd_bike/precache-manifest.559bb5187d4c2ad20f9eae5957c40cb8.js"
+  "/antd_bike/precache-manifest.a5fcd741cc4a22c1a8a766c6d4c989e8.js"
 );
 
 self.addEventListener('message', (event) => {
